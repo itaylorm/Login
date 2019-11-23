5e28aea501e79e7997676fa1502c628fdc1d8fa6
Taylor Maxwell <tmaxwell@I1Biometrics.com>
2019-11-23 06:58:23 -0800

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Taylor Maxwell on 11/23/19.
+//  Copyright © 2019 Taylor Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
